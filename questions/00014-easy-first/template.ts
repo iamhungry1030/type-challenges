d4a5fd867c2d2941d7cdae2a39c464a91a8fabe9
pshdev1030 <imhungry1030@gmail.com>
2024-08-31 19:41:11 +0900

    00014-easy-first

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,5 @@
-type First<T extends any[]> = any
+type First<T extends any[]> = T extends {length:0}?never:T[0]
+
+// 답지보니 이런 풀이도 있음 이게 의도한 답인듯
+// 타입스크립트에서 spread 문법과 infer를 사용해서 평가를 늦추는 방법
+// type First<T extends any[]> = T extends [infer P,...unknown[]]?P:never;
