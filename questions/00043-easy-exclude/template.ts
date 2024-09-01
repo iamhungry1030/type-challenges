dbf8949b7861f1b1c5e3df2b32f571b892e744c6
pshdev1030 <imhungry1030@gmail.com>
2024-09-01 21:51:12 +0900

    00043-easy-exclude

--- questions/00043-easy-exclude/template.ts
+++ questions/00043-easy-exclude/template.ts
@@ -1,1 +1,8 @@
-type MyExclude<T, U> = any
+type MyExclude<T, U> = T extends U? never: T
+
+// 분배 조건부 타입을 배워서 품
+// 조건부 타입이 유니온에 적용될 때 그 유니온 타입의 개별 요소에 조건을 분배하여 평가후 합침
+// https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
+
+// MyExclude<'a' | 'b' | 'c', 'a'>, 'b' | 'c'>의 경우 분배가 아래와 같이 이루어짐
+// ("a" extends U ? never : "a") | ("b" extends U ? never : "b") | ("c" extends U ? never : "c")
